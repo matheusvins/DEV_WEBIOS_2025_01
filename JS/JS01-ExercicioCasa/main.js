366b24f96fb9aea19449206703a4bf6e135745af
matheusvins <mtsvins@outlook.com>
2025-06-15 23:36:16 -0300

    update

---
+++ JS/JS01-ExercicioCasa/main.js
@@ -0,0 +1,8 @@
+let nota1 = 7;
+let nota2 = 8.5;
+let nota3 = 6;
+let nota4 = 9;
+
+let media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+console.log("A média do aluno com as 4 notas é: " + media);
---
+++ JS/JS02-String/JS02-ExercicioCasa/main.js
@@ -0,0 +1,7 @@
+let frase = "Estamos aprendendo JavaScript e seremos futuros Devs";
+
+console.log("Caractere na posição 19:", frase[19]);
+
+console.log("Tamanho da string:", frase.length);
+
+console.log("String em maiúsculo:", frase.toUpperCase());
